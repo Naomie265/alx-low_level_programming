1326e668aaea4e7572e0a274f2a79df081968d1e
Naomi <naomicmzembe13@gmail.com>
2023-04-20 20:54:22 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  main - A program that prtints a line with puts function
+ *  Return: 0 (Succcess
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle, followed by a new line.");
+	return (0);
+}
